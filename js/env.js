defc2494a7b2f23f42e4667e700fccd1e96a163d
S E R A Y A <takahser@users.noreply.github.com>
2023-10-06 09:20:23 +0200

    configure

--- js/env.js
+++ js/env.js
@@ -1,5 +1,5 @@
-const nftAddress = ""; // Paste the address of the NFT collection.
+const nftAddress = "0x5d08dF1A52Fee1E414E3c81da15b817b3CdD1332"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
-const chainId = "0x507"; // Moonbase
-// const chainId = "0x504"; // Moonbeam
+// const chainId = "0x507"; // Moonbase
+const chainId = "0x504"; // Moonbeam
 // const chainId = "0x250"; // Astar
